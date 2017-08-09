0f4b77e7d67ec0a4db0d04e56ee5026f149747de
Abul Basar <abulbasar@users.noreply.github.com>
2017-08-09 14:01:55 +0530

    Update spark_stream_socket.py

--- spark_stream_socket.py
+++ spark_stream_socket.py
@@ -1,9 +1,18 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
-# Before running this script run "nc -lk 9999" 
-# command to open a server socket on port 9999
+"""
+Before running this script run "nc -lk 9999" 
+command to open a server socket on port 9999
 
+Start the spark streaming application by running the following command
+$SPARK_HOME/bin/spark-submit spark_stream_socket.py
+
+You may like to reduce the logging level to WARN in log4j.properties configuration
+found in $SPARK_HOME/conf/log4j.properties. Copy this file from the template if not already present.
+
+
+"""
 sc = SparkContext()
 ssc = StreamingContext(sc, 2)
 lines = ssc.socketTextStream("localhost", 9999)
@@ -16,3 +25,5 @@ lines.pprint()
 
 ssc.start()
 ssc.awaitTermination()
+
+
