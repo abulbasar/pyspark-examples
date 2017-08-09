dd9aa9fea8f66c1191d687294d0dc5c39ea610df
Abul Basar <abulbasar@users.noreply.github.com>
2017-08-10 01:16:10 +0530

    Update spark_kafka_stream.py

--- spark_kafka_stream.py
+++ spark_kafka_stream.py
@@ -7,15 +7,10 @@ sc = SparkContext(appName="PythonStreamingKafka")
 ssc = StreamingContext(sc, 3)
 
 zkQuorum, topic = 'localhost:9092', 'test'
-kvs = KafkaUtils.createStream(ssc, zkQuorum,"my_group", {topic: 3})
+raw = KafkaUtils.createStream(ssc, zkQuorum,"my_group", {topic: 3})
 
-lines = kvs.map(lambda x: x.value)
-counts = (lines.flatMap(lambda line: line.split(" "))
-  .map(lambda word: (word, 1)) 
-  .reduceByKey(lambda a, b: a+b)
- )
-
- counts.pprint()
+lines = raw.map(lambda x: x.value)
+lines.pprint()
 
  ssc.start()
  ssc.awaitTermination()
