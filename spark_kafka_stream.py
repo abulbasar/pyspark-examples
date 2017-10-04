c9d9abe8ac26ac91d6dc9c931b9f20b39e0d8556
Abul Basar <abulbasar@users.noreply.github.com>
2017-10-04 16:30:44 +0800

    Update spark_kafka_stream.py

--- spark_kafka_stream.py
+++ spark_kafka_stream.py
@@ -1,16 +1,19 @@
-import sys
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from pyspark import SparkContext
+from pyspark.storagelevel import StorageLevel
 
+batch_interval = 3
 sc = SparkContext(appName="PythonStreamingKafka")
-ssc = StreamingContext(sc, 3)
+ssc = StreamingContext(sc, batch_interval)
 
-zkQuorum, topic = 'localhost:9092', 'test'
-raw = KafkaUtils.createStream(ssc, zkQuorum,"my_group", {topic: 3})
+zookeeper, topic = 'localhost:2181/kafka', 'intel'
+raw = KafkaUtils.createStream(ssc, zookeeper,"spark_cg", {topic: 3})
+#raw.pprint()
 
-lines = raw.map(lambda x: x.value)
+lines = raw.map(lambda x: x[1])
 lines.pprint()
 
- ssc.start()
- ssc.awaitTermination()
+ssc.start()
+ssc.awaitTermination()
+print("Spark streaming application has been terminated")
