0c93133941fc0c212f3fd46cd0db3c1cff2e6c24
Abul Basar <abulbasar@users.noreply.github.com>
2017-12-03 11:44:01 +0530

    Update random_file_generator.py

--- random_file_generator.py
+++ random_file_generator.py
@@ -6,7 +6,7 @@ n = random.randint(1, 1000)
 delay = 3
 max_files = 1000
 print("This utility continuously create new file that contains random numbers")
-
+i = 0
 while i < max_files:
   lines = [str(random.random()) + "\n" for _ in range(n)]
   file_name = datetime.now().strftime("%s") + ".dat"
