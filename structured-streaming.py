ddefa67bae5811443e3e39dc185b56b8de283941
Abul Basar <abulbasar@users.noreply.github.com>
2018-02-09 14:37:48 +0530

    Update structured-streaming.py

--- structured-streaming.py
+++ structured-streaming.py
@@ -5,28 +5,19 @@ from pyspark.sql.functions import split
 """
 
 Start a socket streaming source using the following command
-
 $ nc -nlk 9999 
 
 Above 9999 is the port number on which socket messages will be published.
-
 Set the logging level to WARN before you launch spark streaming service so that 
-
 streaming messages are not lost in the logs message.  
 
 Open $SPARK_HOME/conf/log4j.properties and set the following
-
 log4j.rootCategory=WARN, console
 
 
 Then start the spark streaming application
-
 $SPARK_HOME/bin/spark-submit structured-streaming.py
-
-
 """
-
-
 spark = (SparkSession
     .builder
     .appName("StructuredStreaming")
