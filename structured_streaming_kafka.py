d2fc09654ea3f41a640b38b19c02acd2393a14e8
Abul Basar <abulbasar@users.noreply.github.com>
2018-02-09 14:53:57 +0530

    Update structured_streaming_kafka.py

--- structured_streaming_kafka.py
+++ structured_streaming_kafka.py
@@ -3,29 +3,36 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 
+"""
+
+Launch spark application
+$ $SPARK_HOME/usr/lib/spark-2.2.0-bin-hadoop2.7/bin/spark-submit \
+--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.0,org.xerial.snappy:snappy-java:1.1.4 \
+structured_streaming_kafka.py
+
+"""
+
+
 spark = (SparkSession
     .builder
     .appName("StructuredStreamingWithKafka")
     .getOrCreate())
-# Subscribe to 1 topic
 
-raw = spark
-  .readStream()
+# Source: subscribe to 1 topic in Kafka
+raw = (spark
+  .readStream
   .format("kafka")
   .option("kafka.bootstrap.servers", "localhost:9092")
   .option("subscribe", "demo")
-  .load()
+  .load())
   
+# Sink: defined console sink
 query = (raw
     .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
     .writeStream
     .format("console")
     .start())
 
-query.awaitTermination()
+spark.streams..awaitAnyTermination()
 
-"""
 
-Launch spark application
-$ bin/spark-sumbit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.0,org.xerial.snappy:snappy-java:1.1.4 structured_streaming_kafka.py
-"""
