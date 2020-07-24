e69e521ecfd1e429324f8f73ab2411b5c2d966ed
Abul Basar <abulbasar@users.noreply.github.com>
2020-07-24 11:36:28 +0530

    Update structured_streaming_kafka.py

--- structured_streaming_kafka.py
+++ structured_streaming_kafka.py
@@ -9,15 +9,13 @@ bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.2.0 str
 
 kafka version: kafka_2.11-2.4.1
 
-Note: this code does not work with spark 2.4.*
+Note: this code has been successfully tested with 2.3.4-bin-hadoop2.7, but it does not work with spark 2.4.*
 
 """
 
 
 def main():
     conf = SparkConf().setMaster("local[*]")
-    conf.set("maxOffsetsPerTrigger", "2000")
-    conf.set("failOnDataLoss", "false")
 
     spark = (SparkSession
              .builder
@@ -32,16 +30,21 @@ def main():
            .format("kafka")
            .option("kafka.bootstrap.servers", "localhost:9092")
            .option("subscribe", topic)
+           .option("startingOffsets", "earliest")
+           .option("maxOffsetsPerTrigger","100")
+           .option("failOnDataLoss", "false")
            .load())
+    
     # transformation
     enriched = raw.selectExpr("*", "CAST(key AS STRING)", "CAST(value AS STRING)")
 
     # sink
-    (enriched.writeStream
-    .format("console")
-    .option("truncate", False)
-    .option("numRows", 1000)
-    .start())
+    (enriched
+     .writeStream
+     .format("console")
+     .option("truncate", False)
+     .option("numRows", 1000)
+     .start())
 
     spark.streams.awaitAnyTermination()
     print("Process is complete")
