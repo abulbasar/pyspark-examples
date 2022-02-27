e7523d66dbe6138d8e278b4d856f350d7fbaa1c8
Abul Basar <abulbasar@users.noreply.github.com>
2022-02-27 10:31:29 +0530

    Update structured_streaming_kafka.py

--- structured_streaming_kafka.py
+++ structured_streaming_kafka.py
@@ -16,7 +16,7 @@ bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.4 str
 
 
 def main():
-    conf = SparkConf().setMaster("local[*]")
+    conf = SparkConf().setIfMissing("spark.master", "local[*]")
 
     spark = (SparkSession
              .builder
