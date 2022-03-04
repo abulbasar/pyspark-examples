3fc1db6751e7762ce601833bf1a68b529e0fad2c
Abul Basar <abulbasar@users.noreply.github.com>
2022-03-04 17:17:33 +0530

    Update json_consumer.py

--- kafka-clients/json_consumer.py
+++ kafka-clients/json_consumer.py
@@ -4,9 +4,15 @@ from kafka import KafkaConsumer
 host = "demo1"
 topic = "t1"
 port = 9092
+group_id = "g1"
 
 consumer = KafkaConsumer(topic,
                          group_id="my_group",
+                         auto_offset_reset = "earliest",
+                         group_id = group_id,
+                         enable_auto_commit = True,
+                         auto_commit_interval_ms = 1000,
+                         max_poll_records = 1000, 
                          bootstrap_servers=[f"{host}:{port}"])
 
 for msg in consumer:
